6a191a3bd48c1fdcb150d10f7c56361fca9e253e
danteandrinolo <112221941+danteandrinolo@users.noreply.github.com>
2026-05-17 19:06:12 +0000

    Deploying to gh-pages from @ danteandrinolo/danteandrinolo.github.io@c2f5ccbe873dc177a36881bc6985722209823fef 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -488,6 +488,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-optimal-transcranial-electrical-stimulation-for-tumor-treating-fields",
+          title: 'Optimal Transcranial Electrical Stimulation for Tumor Treating Fields',
+          description: "Optimization-based transcranial electrical stimulation for glioblastoma treating fields.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/ttfields/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
